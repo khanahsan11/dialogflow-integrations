9a4f97656123b52c79cfdb74efac227c2ce9a200
khanahsan11 <71570674+khanahsan11@users.noreply.github.com>
2020-09-19 21:19:37 -0400

    Update server.js

--- spark/server.js
+++ spark/server.js
@@ -31,9 +31,9 @@ app.use(express.json());
 //Upon closure the webhook is removed from spark
 
 //Insert your values here
-const sparkAccessToken ="Place your spark personal access token here";
+const sparkAccessToken ="ZjJmMzMyZmUtODY5OC00NzRhLTk3ZTYtOWNiZTI0YjVkNDE4MjcyOWRlZmQtMTNk_PF84_0d882151-70b4-4264-a09c-4a599e4494b1";
 const targetUrl = 'Place you servers URL here';
-const projectId = 'Place your dialogflow projectId here';
+const projectId = 'uccteam-ajyv';
 
 const sessionClient = new dialogflowSessionClient(projectId);
 
